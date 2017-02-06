54baa9a97f91381cba1c3c1001742a4a5486d886
CrystalOfLife <nicolai7@godmail.dk>
2017-02-06 15:32:34 +0100

    added data to administrator

--- Cemtrelised Attendance Automation/src/Be/Administrator.java
+++ Cemtrelised Attendance Automation/src/Be/Administrator.java
@@ -9,6 +9,34 @@ package Be;
  *
  * @author Nicolai
  */
-public class Administrator {
+public class Administrator 
+{
+    private String username;
+    private String password;
+
+    public Administrator(String username, String password) 
+    {
+        this.username = username;
+        this.password = password;
+    }
     
+    public String getUsername() 
+    {
+        return username;
+    }
+    
+    public void setUsername(String username) 
+    {
+        this.username = username;
+    }
+    
+    public String getPassword() 
+    {
+        return password;
+    }
+    
+    public void setPassword(String password) 
+    {
+        this.password = password;
+    }
 }
