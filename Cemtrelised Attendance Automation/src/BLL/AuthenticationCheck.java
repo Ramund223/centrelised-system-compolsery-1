5dbe0466815c12ae4586c671de69711c7be26bf4
Airdin93 <emil.johansen93@gmail.com>
2017-03-02 14:22:39 +0100

    FIxed the login :) :+1:

--- Cemtrelised Attendance Automation/src/BLL/AuthenticationCheck.java
+++ Cemtrelised Attendance Automation/src/BLL/AuthenticationCheck.java
@@ -13,6 +13,7 @@ package BLL;
 import Be.Student;
 import GUI.Controller.MainViewController;
 import GUI.Model.UserModel;
+import java.io.IOException;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -46,16 +47,16 @@ public class AuthenticationCheck {
         return INSTANCE;
     }
     
-    public void signIn()
+    public void signIn() throws IOException
     {
 //        mainViewController = MainViewController.getInstance();
         for (Student s : listStudents)
         {
 //            System.out.println("heh");
-            if (s.getUsername().equals(currentUser.getCurrentUserName()))
-            {
-                System.out.println("DUr det???");
-            }
+//            if (s.getUsername().equals(currentUser.getCurrentUserName()))
+//            {
+//                System.out.println("DUr det???");
+//            }
 //            mainViewController = null;
             
 //            if (s.getUsername().equals(mainViewController.getUserName()))
@@ -63,60 +64,57 @@ public class AuthenticationCheck {
 //                        System.out.println("works??");
 //                    }
             
-//            if (s.getUsername().equals(mainViewController.getUserNameField()) && (s.getPassword().equals(passwordField.getText())) && ("Teacher".equals(userNameField.getText())))
-//            {
-//                System.out.println("Warning teacher is in the gibson!");
-//                System.out.println("Logged in as " + userNameField.getText());
-//                Stage stage = new Stage();
-//                Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/TeacherView.fxml"));
-//                Scene scene = new Scene(root);
-//                stage.setTitle("Logged in as " + userNameField.getText());
-//                stage.setScene(scene);
-//                stage.setResizable(false);
-//                stage.show();
-//                userNameField.clear();
-//                passwordField.clear();
+            if (s.getUsername().equals(currentUser.getCurrentUserName()) && (s.getPassword().equals(currentUser.getCurrentPassword())) && ("Teacher".equals(currentUser.getCurrentUserName())))
+            {
+                System.out.println("works???");
+                System.out.println("Warning teacher is in the gibson!");
+                System.out.println("Logged in as " + currentUser.getCurrentUserName());
+                Stage stage = new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/TeacherView.fxml"));
+                Scene scene = new Scene(root);
+                stage.setTitle("Logged in as " + currentUser.getCurrentUserName());
+                stage.setScene(scene);
+                stage.setResizable(false);
+                stage.show();
 //                publicMessageLabel.setText("");
-//                break;
-//            }
-//                else if (s.getUsername().equals(userNameField.getText()) && (s.getPassword().equals(passwordField.getText())))
-//            {
-//                System.out.println("Logged in!");
-//                Stage stage = new Stage();
-//                Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/UserView.fxml"));
-//                Scene scene = new Scene(root);
-//                stage.setTitle("Logged in as " + userNameField.getText());
-//                stage.setScene(scene);
-//                stage.setResizable(false);
-//                stage.show();
-//                userNameField.clear();
-//                passwordField.clear();
+                break;
+            }
+                else if (s.getUsername().equals(currentUser.getCurrentUserName()) && (s.getPassword().equals(currentUser.getCurrentPassword())))
+            {
+                System.out.println("Logged in!");
+                Stage stage = new Stage();
+                Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/UserView.fxml"));
+                Scene scene = new Scene(root);
+                System.out.println("Logged in as " + currentUser.getCurrentUserName());
+                stage.setScene(scene);
+                stage.setResizable(false);
+                stage.show();
 //                publicMessageLabel.setText("");
-//                break;
-//            }
-//                else if (userNameField.getText().isEmpty()) 
-//            {
+                break;
+            }
+                else if (currentUser.getCurrentUserName().isEmpty()) 
+            {
 //                publicMessageLabel.setText("No Username Input!");
-//                break;
-//            }
-//                else if (passwordField.getText().isEmpty()) 
-//            {
+                break;
+            }
+                else if (currentUser.getCurrentPassword().isEmpty()) 
+            {
 //                publicMessageLabel.setText("No Password Input!");
-//                break;
-//            }
-//                else if (s.getUsername().equals(userNameField.getText()) && !s.getPassword().equals(passwordField.getText()))
-//            {
+                break;
+            }
+                else if (s.getUsername().equals(currentUser.getCurrentUserName()) && !s.getPassword().equals(currentUser.getCurrentPassword()))
+            {
 //                publicMessageLabel.setText("Wrong Password!");
-//                break;
-//            }
-//                else if (!s.getUsername().equals(userNameField.getText()))
-//            {
+                break;
+            }
+                else if (!s.getUsername().equals(currentUser.getCurrentUserName()))
+            {
 //                publicMessageLabel.setText("No such user in the database!");
-//            }
-//            else
-//            {
-//                //DidNothing
-//            }
+            }
+            else
+            {
+                //DidNothing
+            }
         }
     }
     
